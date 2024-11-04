2acac25877e0a694bc990e0a3f22ac364edac384
doejohn126 <edennis0003@gmail.com>
2024-11-03 22:23:23 -0500

    Route 1 and other stuff

--- include/constants/follow_me.h
+++ include/constants/follow_me.h
@@ -20,11 +20,11 @@
 // Replace the 0 with a flag in order to use that flag to toggle whether the Player's party
 // will be automatically healed after every follower partner battle.
 // If you want this to always be active without using a flag, replace 0 with ALWAYS.
-#define F_FLAG_HEAL_AFTER_FOLLOWER_BATTLE   0
+#define F_FLAG_HEAL_AFTER_FOLLOWER_BATTLE   ALWAYS
 
 // Replace the 0 with a flag in order to use that flag to toggle whether the follower partner will join you for wild battles.
 // If you want this to always be active without using a flag, replace 0 with ALWAYS.
-#define F_FLAG_PARTNER_WILD_BATTLES         0
+#define F_FLAG_PARTNER_WILD_BATTLES         ALWAYS
 
 // If set to TRUE, two wild Pokemon will show up to the partner battle instead of just one.
 #define FOLLOWER_WILD_BATTLE_VS_2           TRUE
